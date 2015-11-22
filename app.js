05c77488b267a81310c94c8c0590d87e23a22813
ygong10 <yichenggong567@gmail.com>
2015-11-22 05:09:33 -0600

    shet load of stuff

--- app.js
+++ app.js
@@ -15,22 +15,51 @@ function getRandomColor() {
 app.get('/', function(req, res){
 	 res.sendFile(__dirname + '/index.html');
 });
+
+<!-- //testing counter for classes -->
+var cs225_count = 0;
+var cs241_count = 0;
+var cs374_count = 0;
+var cs225_rating = 0;
+var cs241_rating = 0;
+var cs374_rating = 0;
+
+//end of test
 var id = 1;
+var online = 0;
 var random_color = getRandomColor();
 io.on('connection', function(socket){
 	var my_id = id ;
 	var rand_color = random_color;
-	io.emit('user connected', ('Anon#' + my_id).fontcolor(rand_color) + ' is connected')
+	io.emit('user connected', ('Anon#' + my_id).fontcolor(rand_color) + ' is connected');
+
+	//count # of people online
+		online = online + 1;
+		io.emit('one more user', online);
+
 	socket.on('chat message', function(msg){
 		var test = "Anon#"+ my_id;
 		var test_it = test.fontcolor(rand_color);
-		io.emit('chat message', test_it + ": " + msg);
+		if (msg.toString().toLowerCase() == "cat") {
+			msg = "https://cdn1.vox-cdn.com/thumbor/vgHtNlYCemvKR2WljMqRfF5tkwo=/0x32:1024x608/1050x591/cdn0.vox-cdn.com/uploads/chorus_image/image/45329786/GrouponCat.0.jpg";
+			io.emit('image message', msg);
+		}
+		else {
+			io.emit('chat message', test_it + ": " + msg);
+		}
 	});
+
 	socket.on('disconnect', function() {
 		var test = "Anon#"+ my_id;
 		var test_it = test.fontcolor(rand_color);
 		io.emit('user disconnect', test_it + " has disconnected");
+		online--;
+		io.emit('one less user', online);
 	});
+	//testing for cs
+
+	//end of cs test
+
 	id++;
 	random_color = getRandomColor();
 });
